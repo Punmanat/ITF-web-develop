8d58d8f5317b63fbe90f9741876ab52d1bc3ad67
Pannita Hamego <32834828+Pannita2212@users.noreply.github.com>
2017-11-15 18:42:40 +0700

    Update bathroom_user.php

--- project_itf/bathroom_user.php
+++ project_itf/bathroom_user.php
@@ -15,35 +15,41 @@
         <div class="row">
             <div class="col-sm-4">
                 <img class='' src="catalog/bathroom/1.png" width="300" height="200">
-                <p>Price : 1000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : MAAX ® Orchestra Freestanding Bathtub</p>
+                <p>Price : 31229฿</p>
+                <p>Description : Sleek design and installation simplicity are in full concert with the Orchestra freestanding bathtub. Combine luxury with practicality, this minimalist symmetrical look bathtub is designed for optimal comfort with a maximized bathing well for a deep and relaxing bathing experience. With the MAAX innovative adjustable legs and practical plumbing feature the Orchestra tub is the perfect tub for your renovation project.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/bathroom/2.png" width="300" height="200">
-                <p>Price : 35000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : Pedestal Express All-In-One Box White Pedestal Sink</p>
+                <p>Price : 1959฿</p>
+                <p>Description : Briggs Pedestal Express includes white pedestal sink, white pedestal base, chrome faucet and pop-up drain all-in-one box. Briggs™ </p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/bathroom/3.png" width="300" height="200">
-                <p>Price : 60000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : Orchard Wye thermostatic bar valve shower system</p>
+                <p>Price : 6230฿</p>
+                <p>Description : This shower system from our Wye range is a great way to add contemporary style to your new shower enclosure or shower bath. </p>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-4">
                 <img class='' src="catalog/bathroom/4.png" width="300" height="200">
-                <p>Price : 60000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : American Standard Fairfield Standard Elongated Toilet in White</p>
+                <p>Price : 8999฿</p>
+                <p>Description : Engineered to be admired in more ways than one, this one-piece, low-profile toilet would make a striking addition to any modern or minimalist bathroom. Its high-volume 3? flush valve provides a powerful flush, and the longer seat and bowl provide greater comfort.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/bathroom/5.png" width="300" height="200">
-                <p>Price : 60000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : Mira Mode dual rear fed digital shower pumped</p>
+                <p>Price : 19950฿</p>
+                <p>Description : Mira Mode dual rear fed digital shower pumped With the new digital shower from Mira Showers, the next generation of showering is here. Combining flawless design with uncompromising performance, Mira Mode is designed to perfectly complement a contemporary bathroom and lifestyle</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/bathroom/6.png" width="300" height="200">
-                <p>Price : 60000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name: Lyons Elite™ White Left Hand Drain Soaking Bathtub</p>
+                <p>Price : 11399฿</p>
+                <p>Description : Immerse yourself in this extra deep bathtub. Built-in arm rests and lumbar support give you a relaxing bathing experience. Quick Ship available for Almond and Biscuit.</p>
             </div>
         </div>
         <?php include 'footer_user.php'; ?>
