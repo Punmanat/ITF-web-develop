8583f8b1b17d4aa67885b3f0cdebd2acc5e4e598
Pannita Hamego <32834828+Pannita2212@users.noreply.github.com>
2017-11-15 20:51:34 +0700

    Update kitchen_user.php

--- project_itf/kitchen_user.php
+++ project_itf/kitchen_user.php
@@ -15,93 +15,109 @@
         <div class="row">
             <div class="col-sm-4">
                 <img class='' src='catalog/kitchen/1.jpg' width="300" height="200">
-                <p>Price : 1000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : Wall cabinet with 2 doors, white, Ringhult white</p>
+                <p>Price : 9500฿</p>
+                <p>Description : Snap-on hinges can be mounted onto the door without screws, and you can easily remove the door for cleaning.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/kitchen/2.jpg" width="300" height="200">
-                <p>Price : 3000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : Society Shaker Canvas</p>
+                <p>Price : 16950฿</p>
+                <p>Description : The Society Shaker Canvas offers a well-designed and classy look. These cabinets feature a jaw-dropping painted black finish. The Society Shaker Canvas cabinets feature quality construction and excellent craftsmanship. If you're looking for a stylish look to enhance your kitchen then look no further!</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/kitchen/3.jpg" width="300" height="200">
-                <p>Price : 6000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : Base cabinet , white Maximera, Bodbyn off-white</p>
+                <p>Price : 19990฿</p>
+                <p>Description : Different wall materials require different types of fasteners. Use fasteners suitable for the walls in your home. Requires a knob or a handle</p>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-4">
                 <img class='' src="catalog/kitchen/4.jpg" width="300" height="200">
-                <p>Price : 10000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : Base cabinet f/DOMSJÖ white, Veddinge white</p>
+                <p>Price : 7990฿</p>
+                <p>Description : The door damper prevents your cabinet door from slamming by catching the moving door so that it closes slowly, gently and silently.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/kitchen/5.jpg" width="300" height="200">
-                <p>Price : 30000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : Base cabinet with drawer, white Maximera, Voxtorp high-gloss light beige</p>
+                <p>Price : 6590฿</p>
+                <p>Description : MAXIMERA drawer is a smooth-running, full-extension drawer with built-in dampers so that it closes slowly, softly, and quietly.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/kitchen/6.jpeg" width="300" height="200">
-                <p>Price : 76000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : SEKTION cabinet , black, Laxarby black-brown</p>
+                <p>Price : 12990฿</p>
+                <p>Description : Snap-on hinges can be mounted onto the door without screws, and you can easily remove the door for cleaning.</p>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-4">
                 <img class='' src="catalog/kitchen/7.jpg" width="300" height="200">
-                <p>Price : 10000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : VARIERA spice/Utensil/knife tray, bamboo</p>
+                <p>Price : 899฿</p>
+                <p>Description : clever interiors inside drawers and cabinets keep things organized. A helpful tip is to keep things close to where they are used as this enables a smooth workflow.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/kitchen/8.jpg" width="300" height="200">
-                <p>Price : 30000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : Copenhagen wall storage</p>
+                <p>Price : 4490฿</p>
+                <p>Description : usable space with this clever configuration of cubbies. Solid acacia wood tower alternates small and large cubes/shelves creating tons of storage space. Designed to sit either side up, depending on your space. </p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/kitchen/9.jpg" width="300" height="200">
-                <p>Price : 76000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : OMAR 2 section shelving unit</p>
+                <p>Price : 3550฿</p>
+                <p>Description : OMAR shelf unit makes your pantry look like a professional kitchen. But it isn’t only a kitchen dweller, it works equally well across the home. Add sections and adjust the shelf heights to fit your needs.</p>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-4">
                 <img class='' src="catalog/kitchen/10.jpg" width="300" height="200">
-                <p>Price : 10000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : FALSTERBO Wall shelf</p>
+                <p>Price : 1990฿</p>
+                <p>Description : Different wall materials require different types of fasteners. Use fasteners suitable for the walls in your </p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/kitchen/11.jpg" width="300" height="200">
-                <p>Price : 30000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : SKOGSTA Wall shelf, acacia</p>
+                <p>Price : 2200฿</p>
+                <p>Description : Solid wood is a durable natural material. The shelf becomes one with the wall thanks to the concealed mounting hardware.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/kitchen/12.jpg" width="300" height="200">
-                <p>Price : 76000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : TORSBY / LEIFARNE Table and 6 chairs, high gloss white</p>
+                <p>Price : 12500฿</p>
+                <p>Description : The high-gloss surface reflects light and gives a vibrant look.</p>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-4">
                 <img class='' src="catalog/kitchen/13.jpg" width="300" height="200">
-                <p>Price : 10000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : EKEDALEN / HENRIKSDAL Table and 6 chairs, Glose black</p>
+                <p>Price : 18500฿</p>
+                <p>Description : A durable dining set that makes it easy to have big dinners. A single person can extend the table and there’s plenty of room for chairs since the legs are always located at the corners of the table.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/kitchen/14.jpg" width="300" height="200">
-                <p>Price : 30000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : STORNÄS / HENRIKSDAL, Nolhaga gray-beige</p>
+                <p>Price : 28800฿</p>
+                <p>Description : Extendable dining table with two extra leaves seats 4-6, so you can quickly and easily adapt the table to your needs.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/kitchen/15.jpg" width="300" height="200">
-                <p>Price : 76000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : Branka Dining Table</p>
+                <p>Price : 25990฿</p>
+                <p>Description : Make a bold statement in your casual or formal dining room with this table. It features a large scaled X base design with rectangular glass top. The base is crafted from ash veneers and finished in a deep gray tone. The unique base will create a modern look for your dining room.</p>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-4">
                 <img class='' src="catalog/kitchen/16.jpg" width="300" height="200">
-                <p>Price : 10000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : Janet Dining Set</p>
+                <p>Price : 10500฿</p>
+                <p>Description : Complete your dining room ensemble or eat-in kitchen with this chic set, featuring stylish openwork details and contrasting white and black finishes.</p>
             </div>
         </div>
         <?php include 'footer_user.php'; ?>
