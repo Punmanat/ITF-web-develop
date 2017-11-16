c52e3a989047d34f875c296d5da15d9b9c6f8d67
Yonlada Prasert <60070077@kmitl.ac.th>
2017-11-16 17:03:46 +0700

    Livingroom

--- project_itf/livingroom_user.php
+++ project_itf/livingroom_user.php
@@ -15,35 +15,41 @@
         <div class="row">
             <div class="col-sm-4">
                 <img class='' src='catalog/living_room/1.png' width="300" height="200">
-                <p>Price : 1000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : 3-seat sofa</p>
+                <p>Price : 21,000</p>
+                <p>Description : you can sit and laydown.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/living_room/2.png" width="300" height="200">
-                <p>Price : 3000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : 4 - seat sofa</p>
+                <p>Price : 32,000</p>
+                <p>Description : you can sit and laydown.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/living_room/3.png" width="300" height="200">
-                <p>Price : 6000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : TV storage combination</p>
+                <p>Price : 26,650</p>
+                <p>Description : Telvision table and you can keep stuff above and beside shelves.</p>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-4">
                 <img class='' src="catalog/living_room/4.png" width="300" height="200">
-                <p>Price : 10000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : TV storage combination</p>
+                <p>Price : 34,650 </p>
+                <p>Description : Telvision table and you can keep stuff above and at the side shelves.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/living_room/5.png" width="300" height="200">
-                <p>Price : 30000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : TV bench with drawers</p>
+                <p>Price : 10,450 </p>
+                <p>Description : You can place TV and kepp your stuff in the shelves below.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/living_room/6.png" width="300" height="200">
-                <p>Price : 76000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : TV bench with drawers</p>
+                <p>Price : 9,990</p>
+                <p>Description : You can place TV and keep your stuff in the shelves.</p>
             </div>
         </div>
         <?php include 'footer_user.php'; ?>
