204dfded40c0691c257e51e942bffb8b1a8e0ffb
Pannita Hamego <32834828+Pannita2212@users.noreply.github.com>
2017-11-19 15:15:08 +0700

    Update bedroom_user.php

--- project_itf/bedroom_user.php
+++ project_itf/bedroom_user.php
@@ -15,64 +15,75 @@
         <div class="row">
             <div class="col-sm-4">
                 <img class='' src="catalog/bedroom/2.png" width="300" height="200">
-                <p>Price : 1000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : NESTTUN</p>
+                <p>Price : 5,290</p>
+                <p>Description : The durable steel construction is coupled with a net-patterned headboard creating a light and airy expression. A simple design that mixes nicely with a variety of other styles and furniture.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/bedroom/3.png" width="300" height="200">
-                <p>Price : 35000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : KOPARDAL</p>>
+                <p>Price : 4,590</p>
+                <p>Description : You don’t have to look around in antique shops to find a classic steel bed frame. This simple, timeless design mixes nicely with a variety of other styles and furniture.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/bedroom/4.png" width="300" height="200">
+                <p>Name : KOPARDAL</p>>
                 <p>Price : 60000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Description : You don’t have to look around in antique shops to find a classic steel bed frame. This simple, timeless design mixes nicely with a variety of other styles and furniture.</p>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-4">
                 <img class='' src="catalog/bedroom/5.png" width="300" height="200">
-                <p>Price : 60000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : LINNMON</p>>
+                <p>Price : 5,550</p>
+                <p>Description : The table top is covered with a matte paint that protects it against knocks and scratches while making the surface soft and smooth.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/bedroom/6.png" width="300" height="200">
-                <p>Price : 60000</p>
-                <p>Description : xxxxxxx</p>
+                <p> Name : HEMNES </p>
+                <p>Price : 11,590</p>
+                <p>Description : Sustainable beauty from sustainably-sourced solid wood, a durable and renewable material that maintains its genuine character with each passing year. Combines with the other furniture in the HEMNES series.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/bedroom/7.png" width="300" height="200">
-                <p>Price : 60000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : MALM</p>>
+                <p>Price : 4,990</p>
+                <p>Description : It’s really a dressing table – with space for make-up and jewellery inside. But, works just as well as a desk, a place to unload keys or mail in the entrance or to pile magazines behind the couch.</p>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-4">
                 <img class='' src="catalog/bedroom/8.png" width="300" height="200">
-                <p>Price : 60000</p>
-                <p>Description : xxxxxxx</p>
+                <P>Name :DOMBAS </P>
+                <p>Price : 5,500</p>
+                <p>Description : Adjustable shelves make it easy to customise the space according to your needs.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/bedroom/9.png" width="300" height="200">
-                <p>Price : 60000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : PAX</p>
+                <p>Price : 21,000</p>
+                <p>Description : This wardrobe combination is just as good looking on the outside as it is clever on the inside. Guaranteed to outlast years of outfits.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/bedroom/10.png" width="300" height="200">
-                <p>Price : 60000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : PAS</p>
+                <p>Price : 29,120</p>
+                <p>Description : This wardrobe combination is just as good looking on the outside as it is clever on the inside. Guaranteed to outlast years of outfits.</p>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-4">
                 <img class='' src="catalog/bedroom/11.png" width="300" height="200">
-                <p>Price : 60000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : PAW</p>
+                <p>Price : 29,370</p>
+                <p>Description : This wardrobe combination is just as good looking on the outside as it is clever on the inside. Guaranteed to outlast years of outfits.</p>
             </div>
             <div class="col-sm-4">
                 <img class='' src="catalog/bedroom/12.png" width="300" height="200">
-                <p>Price : 60000</p>
-                <p>Description : xxxxxxx</p>
+                <p>Name : KALM</p>
+                <p>Price : 4,990</p>
+                <p>Description : It’s really a dressing table – with space for make-up and jewellery inside. But, works just as well as a desk, a place to unload keys or mail in the entrance or to pile magazines behind the couch.</p>
             </div>
         </div>
         <?php include 'footer_user.php'; ?>
